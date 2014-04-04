3b80f01fd837e31b5aa66ed322d6f7cf59ac3b22
Priyank Gupta <priyank.gupta@vinsol.com>
2014-04-04 17:37:12 +0530

    minor change

--- db/migrate/20140404114919_change_datatype_for_amount_in_spree_plans.rb
+++ db/migrate/20140404114919_change_datatype_for_amount_in_spree_plans.rb
@@ -1,6 +1,6 @@
 class ChangeDatatypeForAmountInSpreePlans < ActiveRecord::Migration
   def up
-    change_column :spree_plans, :amount, :decimal
+    change_column :spree_plans, :amount, :decimal, :scale => 2, :precision => 8
   end
 
   def down
