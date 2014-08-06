585860ddf11ada8357b958d45542c17061696efb
Priyank Gupta <priyank.gupta@vinsol.com>
2014-08-06 19:09:12 +0530

    change #find_or_create_stripe_customer to create independent customer without using token

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,9 +1,13 @@
 Spree::User.class_eval do
   has_many :subscriptions
 
-  def find_or_create_stripe_customer(token)
+  def find_or_create_stripe_customer(token=nil)
     return api_customer if stripe_customer_id?
-    customer = Stripe::Customer.create(description: email, email: email, card: token)
+    customer = if token
+      Stripe::Customer.create(description: email, email: email, card: token)
+    else
+      Stripe::Customer.create(description: email, email: email)
+    end
     update_column(:stripe_customer_id, customer.id)
     customer
   end
